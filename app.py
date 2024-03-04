a7f36ba5473625c88b0b3b8409cec0d371430938
Anudeep K K <anudeep2003@gmail.com>
2024-03-04 13:41:06 +0530

    configration mysql

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ app.config['MYSQL_HOST'] = MYSQL_HOST
 app.config['MYSQL_USER'] = MYSQL_USER
 app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
 app.config['MYSQL_DB'] = MYSQL_DB
+app.config['MYSQL_PORT'] = 3306  
+app.config['MYSQL_UNIX_SOCKET'] = None
+
 
 
 mysql = MySQL(app)
